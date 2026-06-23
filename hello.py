2e570e9436e33e3a75286f5736e7793b1fe522eb
Rida4142 <waheedrida56@gmail.com>
2026-06-23 22:50:33 +0500

    Revise basic syntax and concepts

---
+++ hello.py
@@ -0,0 +1,17 @@
+name="Rida"
+#this is age 
+
+age=25  #this is age 
+"""
+This is a multi-line comment
+"""
+
+fist_name="Rida"
+last_name="Khan"
+
+full_name=fist_name+ " " +last_name 
+last_name=full_name* 12
+
+len(fist_name) #this will give the length of the string
+
+len(last_name) #this will give the length of the string
