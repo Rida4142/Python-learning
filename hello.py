f8e5feec2fdc11abad796b9f69540f6394b5d116
Rida4142 <waheedrida56@gmail.com>
2026-06-27 23:54:27 +0500

    Learnt basic libraries in python cont

--- hello.py
+++ hello.py
@@ -49,3 +49,9 @@ def greet_bob(last_name,first_name="Bob"):
     print("Hello"+" "+first_name+" "+last_name)
 
 greet_bob(first_name="Khan",last_name="alice")
+
+
+print("helo")
+name=input("Enter your name: ")
+
+print("lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.*29")
